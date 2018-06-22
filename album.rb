e1c37b2081121926b9681566c4a13bf6834b4a8d
Sonali Sharma <sonalisharma58521@gmail.com>
2018-06-22 20:31:43 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -20,7 +20,7 @@ def date_of_album
   @date
 end
 end
-
+end
 shawn_mendes = Album.new("SHAWN MENDES","2018")
 taylor_swift= Album.new("Reputation","2017")
 drake = Album.new("Scoprion","2018")             # these are all local variables
@@ -28,4 +28,3 @@ drake = Album.new("Scoprion","2018")             # these are all local variables
 # puts "The album: #{shawn_mendes.name_of_album} was released in #{shawn_mendes.date_of_album}"
 
 puts "#{Album.count}" # tells amount of albums mentioned if delte this and line 2 7 10 and 11 then un hash line 27 to get album info
-end
